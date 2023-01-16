c2679c90635fd57d6007e5c82b77cecb4ca41757
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-16 14:09:27 -0500

    measure and print elapsed time for integer addition

--- project.cpp
+++ project.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
 using namespace std;
 
 void integer_addition() //function whose execution time is to be measured
@@ -10,15 +11,18 @@ void integer_addition() //function whose execution time is to be measured
   int b = 4;
   int sum;
 
+  auto start_time = chrono::steady_clock::now();
   while (loop_no < loop_max)
   {
     sum = a + b;
     loop_no ++;
   }
+  auto end_time = chrono::steady_clock::now();
+  cout << "Elapsed time in seconds: " << chrono::duration_cast<chrono::seconds>(end_time - start_time).count() << " sec" << endl;
 }
 
 int main()
 {
-
+  integer_addition();
   return 0;
 }
