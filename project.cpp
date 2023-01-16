4169a04ce8474cd841b953c3346db05aafcf2500
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-16 13:55:21 -0500

    declared integer addition benchmark function, decleared variables used in it

--- project.cpp
+++ project.cpp
@@ -2,6 +2,16 @@
 #include <fstream>
 using namespace std;
 
+void integer_addition() //function whose execution time is to be measured
+{
+	long long loop_max = 10000000000;
+  long long loop_no = 0;
+  long a = 2;
+  long b = 4;
+
+
+}
+
 int main()
 {
 
