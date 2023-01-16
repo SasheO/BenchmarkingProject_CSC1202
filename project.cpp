6b6f67264bbfaf1f739d8cf6ce8b5cec6c7aa8e1
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-16 13:57:04 -0500

    changed variable type to integer

--- project.cpp
+++ project.cpp
@@ -6,9 +6,9 @@ void integer_addition() //function whose execution time is to be measured
 {
 	long long loop_max = 10000000000;
   long long loop_no = 0;
-  long a = 2;
-  long b = 4;
-  long sum;
+  int a = 2;
+  int b = 4;
+  int sum;
 
   while (loop_no < loop_max)
   {
