aebc5f8ee8cc46f98063652802c447bc481defcb
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-16 14:19:44 -0500

    changed function name, declared variables for result of division and multiplication

--- project.cpp
+++ project.cpp
@@ -3,19 +3,21 @@
 #include <chrono>
 using namespace std;
 
-void integer_addition() //function whose execution time is to be measured
+void integer_operations() //function whose execution time is to be measured
 {
-	long long loop_max = 10000000000;
+	long long loop_max = 1000000000;
   long long loop_no = 0;
   int a = 2;
   int b = 4;
   int sum;
+  int quotient;
+  int product;
 
   auto start_time = chrono::steady_clock::now();
   while (loop_no < loop_max)
   {
-    sum = a + b;
-    loop_no ++;
+
+    
   }
   auto end_time = chrono::steady_clock::now();
   cout << "Elapsed time in seconds: " << chrono::duration_cast<chrono::seconds>(end_time - start_time).count() << " sec" << endl;
@@ -23,6 +25,6 @@ void integer_addition() //function whose execution time is to be measured
 
 int main()
 {
-  integer_addition();
+  integer_operations();
   return 0;
 }
