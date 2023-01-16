5c7acbdb682b065ba9f2c412916c7d665aba0418
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-16 14:21:06 -0500

    created loops for each operation, updated output message

--- project.cpp
+++ project.cpp
@@ -17,10 +17,21 @@ void integer_operations() //function whose execution time is to be measured
   while (loop_no < loop_max)
   {
 
+     for (int i=0; i<10; i++){ // addition
+      sum = a + b;
+    }
+
+    for (int i=0; i<5; i++){ // multiplication
+      product = a * b;
+    }
+    for (int i=0; i<; i++){ // division
+      quotient = a / b;
+    }
+    loop_no ++;
     
   }
   auto end_time = chrono::steady_clock::now();
-  cout << "Elapsed time in seconds: " << chrono::duration_cast<chrono::seconds>(end_time - start_time).count() << " sec" << endl;
+  cout << "Time for integer operations: " << chrono::duration_cast<chrono::seconds>(end_time - start_time).count() << " sec" << endl;
 }
 
 int main()
