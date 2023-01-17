2380c52bd08465b0f1cb4788c4688c8a3f34d175
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-17 09:05:36 -0500

    changed unit of memory_operations output to ns

--- project.cpp
+++ project.cpp
@@ -70,7 +70,7 @@ void floating_point_operations() //function whose execution time is to be measur
 // TODO: fix this. output given is 1 ms??
 void memory_operations(){
   int *ptr;
-  int arr[500000];
+  int arr[50000];
   ptr = arr; // store the address of the first element of arr in ptr
   long long loop_max = 500000;
   long long loop_no = 0;
@@ -78,7 +78,7 @@ void memory_operations(){
   int b = 11;
 
   auto start_time = chrono::steady_clock::now();
-  for (int i = 0; i < 10000; i++){ // runs 10^4 times
+  for (int i = 0; i < 100000; i++){ // runs 10^4 times
     while (loop_no < loop_max) // while loop runs 5 * 10^5 times
     {
       a = *ptr; // read from memory
@@ -91,9 +91,11 @@ void memory_operations(){
     }
 
     ptr = arr; // set pointer back to beginning of array in next loop
+  
   }
+
   auto end_time = chrono::steady_clock::now();
-  cout << "Time for memory operations: " << chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count() << " ms" << endl;
+  cout << "Time for memory operations: " << chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count() << " ns" << endl;
   
 
 }
