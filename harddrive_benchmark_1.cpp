cac3bb52e341c63cd7cd79c99d4b1701b6d65845
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 18:33:16 -0500

    included import library statements in harddrive_benchmark1.cpp

--- harddrive_benchmark_1.cpp
+++ harddrive_benchmark_1.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+using namespace std;
+
 int main(){
     char outplaceholder[100]; // create static array of 100 chars 1 byte each
     char inplaceholder[100]; // create static array of 100 chars 1 byte each
