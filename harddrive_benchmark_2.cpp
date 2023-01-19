6bc12d9d310f90a0c27158110c5988ea11d54707
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 20:07:49 -0500

    included while loops for reading and writing to file
    in harddrive_benchmark_2.cpp

--- harddrive_benchmark_2.cpp
+++ harddrive_benchmark_2.cpp
@@ -11,7 +11,19 @@ int main(){
     long long loop_max = 100000;
     long long loop_no = 0;
 
-    
+    auto start_time = chrono::steady_clock::now();
+    while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
+    outputFile << a_10000bytes;
+    loop_no++;
+    }
+    outputFile.close();
+
+    while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
+    inputFile.read(b_10000bytes, sizeof(b_10000bytes) - 1);
+    loop_no++;
+    }
+    auto end_time = chrono::steady_clock::now();
+    inputFile.close();
 
     cout << "Time for hard drive benchmark 2: " << chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count() << " ns" << endl;
 
