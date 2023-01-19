02d4f88eee3b140fac624c6c6a727f4fa7356f3f
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 19:37:41 -0500

    included instructions to write to file
    commented out instructions to read form file

--- harddrive_benchmark_1.cpp
+++ harddrive_benchmark_1.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main(){
     string a_100bytes = string(100, 'a');
-    ofstream outputFile("../out.txt"\);
+    ofstream outputFile("../out.txt");
     ifstream inputFile("../data.txt");
     long long loop_max = 10000000;
     long long loop_no = 0;
 
     auto start_time = chrono::steady_clock::now();
     while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
-    outputFile.write(outplaceholder, 100);
-    inputFile.read(inplaceholder, 100);
+    outputFile << a_100bytes;
+    // inputFile.read(inplaceholder, 100);
     loop_no++;
     }
     auto end_time = chrono::steady_clock::now();
