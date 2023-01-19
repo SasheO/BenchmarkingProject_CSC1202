75f94f58c674d6ebf59270d72b71b1daba72962c
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 20:06:14 -0500

    deleted while loop in harddrive_benchmark2.cpp

--- harddrive_benchmark_2.cpp
+++ harddrive_benchmark_2.cpp
@@ -11,15 +11,7 @@ int main(){
     long long loop_max = 100000;
     long long loop_no = 0;
 
-    auto start_time = chrono::steady_clock::now();
-    while (loop_no<loop_max){ // write to file 10^5 times, 10000 byte per time
-    outputFile.write(outplaceholder, 10000);
-    inputFile.read(inplaceholder, 10000);
-    loop_no++;
-    }
-    auto end_time = chrono::steady_clock::now();
-    outputFile.close();
-    inputFile.close();
+    
 
     cout << "Time for hard drive benchmark 2: " << chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count() << " ns" << endl;
 
