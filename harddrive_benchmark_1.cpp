dd97934c493c1ed81cf507369d6064d0fa44a86b
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 19:45:21 -0500

    moved ifstream declaration and closing file to after writing to file, changed file names in declarations to data.txt

--- harddrive_benchmark_1.cpp
+++ harddrive_benchmark_1.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main(){
     string a_100bytes = string(100, 'a');
-    ofstream outputFile("../out.txt");
-    ifstream inputFile("../data.txt");
+    ofstream outputFile("../data.txt");
     long long loop_max = 10000000;
     long long loop_no = 0;
 
     auto start_time = chrono::steady_clock::now();
     while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
     outputFile << a_100bytes;
-    // inputFile.read(inplaceholder, 100);
     loop_no++;
     }
     auto end_time = chrono::steady_clock::now();
     outputFile.close();
+
+    ifstream inputFile("../data.txt");
     inputFile.close();
 
     cout << "Time for hard drive benchmark 1: " << chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count() << " ns" << endl;
