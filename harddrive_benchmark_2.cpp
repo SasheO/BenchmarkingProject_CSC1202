2766fb0ff9bc71e09ba2c41a338df08c97dff2ee
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-18 20:08:38 -0500

    updated variable name and comments

--- harddrive_benchmark_2.cpp
+++ harddrive_benchmark_2.cpp
@@ -7,18 +7,18 @@ int main(){
     string a_10000bytes = string(10000, 'a');
     ofstream outputFile("../data.txt");
     ifstream inputFile("../data.txt");
-    char b_100bytes[10001]; // 101 because of 100 bytes and null char
+    char b_10000bytes[10001]; // 10001 because of 10000 bytes and null char
     long long loop_max = 100000;
     long long loop_no = 0;
 
     auto start_time = chrono::steady_clock::now();
-    while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
+    while (loop_no<loop_max){ // write to file 10^5 times, 10000 byte per time
     outputFile << a_10000bytes;
     loop_no++;
     }
     outputFile.close();
 
-    while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
+    while (loop_no<loop_max){ // write to file 10^5 times, 10000 byte per time
     inputFile.read(b_10000bytes, sizeof(b_10000bytes) - 1);
     loop_no++;
     }
