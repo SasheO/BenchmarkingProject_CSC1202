4aafc9d0b4f4488eed7c95dd242f9ab29103dda0
SasheO <76496417+SasheO@users.noreply.github.com>
2023-01-29 16:40:46 -0500

    moved ifstream declaration to after file has been created

--- harddrive_benchmark_1.cpp
+++ harddrive_benchmark_1.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(){
     string a_100bytes = string(100, 'a');
     ofstream outputFile("../data.txt");
-    ifstream inputFile("../data.txt");
     char b_100bytes[101]; // 101 because of 100 bytes and null char
     long long loop_max = 10000000;
     long long loop_no = 0;
@@ -19,6 +18,7 @@ int main(){
     outputFile.close();
 
     loop_no = 0;
+    ifstream inputFile("../data.txt");
 
     while (loop_no<loop_max){ // write to file 10^7 times, 100 byte per time
     inputFile.read(b_100bytes, sizeof(b_100bytes) - 1);
--- harddrive_benchmark_2.cpp
+++ harddrive_benchmark_2.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(){
     string a_10000bytes = string(10000, 'a');
     ofstream outputFile("../data.txt");
-    ifstream inputFile("../data.txt");
     char b_10000bytes[10001]; // 10001 because of 10000 bytes and null char
     long long loop_max = 100000;
     long long loop_no = 0;
@@ -19,6 +18,7 @@ int main(){
     outputFile.close();
 
     loop_no = 0;
+    ifstream inputFile("../data.txt");
 
     while (loop_no<loop_max){ // write to file 10^5 times, 10000 byte per time
     inputFile.read(b_10000bytes, sizeof(b_10000bytes) - 1);
